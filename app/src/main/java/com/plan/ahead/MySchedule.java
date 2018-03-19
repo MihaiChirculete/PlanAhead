e88cea77d77acb3d4727ecaa001680b491095d30
MihaiChirculete <mihai.chirculete98@gmail.com>
2018-03-19 10:45:01 +0200

    Added dialog for adding new events

--- app/src/main/java/com/plan/ahead/MySchedule.java
+++ app/src/main/java/com/plan/ahead/MySchedule.java
@@ -1,5 +1,6 @@
 package com.plan.ahead;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.RectF;
@@ -51,7 +52,7 @@ public class MySchedule extends Fragment {
         fabAddEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity().getApplicationContext(), "Add event (NOT IMPLEMENTED)", Toast.LENGTH_SHORT).show();
+                addNewEvent(view);
             }
         });
 
@@ -101,6 +102,34 @@ public class MySchedule extends Fragment {
 
             }
         });
+    }
+
+
+    void addNewEvent(View v)
+    {
+        // custom dialog
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_add_event);
+        dialog.setTitle("Add new event");
+
+        // set the custom dialog components - text, image and button
+        /*
+        TextView text = (TextView) dialog.findViewById(R.id.text);
+        text.setText("Android custom dialog example!");
+        ImageView image = (ImageView) dialog.findViewById(R.id.image);
+        image.setImageResource(R.drawable.ic_launcher);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        // if button is clicked, close the custom dialog
+        dialogButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        */
+
+        dialog.show();
     }
 
 }
